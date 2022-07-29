f946085d76c2865952a914b67918d8a722de3cbb
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-07-29 20:15:13 +0700

    perf: remove code unused

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import reactLogo from './assets/react.svg';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(0);
 
   return (
     <div className="App">
